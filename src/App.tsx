6aa5b823f7453c1787dc46dd8223e43ccbfb85d3
alya1007 <alexandra.konjevic@isa.utm.md>
2024-04-25 17:41:17 +0300

    feat(style): add theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import "./App.css";
-import {movies} from "./data/movies"
+// import "./App.css";
+import ThemeSwitch from "./components/ThemeSwitch";
+import { movies } from "./data/movies";
 
 function App() {
 	localStorage.setItem("movies", JSON.stringify(movies));
 
 	return (
-		<>
-			<h1 className="text-7xl font-bold underline">Hello world!</h1>
-		</>
+		<div className="app bg-slate-200 dark:bg-black h-screen w-full p-6 transition-colors duration-500 ease-in-out">
+			<div className="flex flex-col items-center h-full justify-between">
+				<h1 className="text-slate-800 ">Hello World</h1>
+				<ThemeSwitch />
+			</div>
+		</div>
 	);
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+	darkMode: "class",
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		fontFamily: {
+			sans: ["Graphik", "sans-serif"],
+			serif: ["Merriweather", "serif"],
+		},
+		extend: {
+			spacing: {
+				"8xl": "96rem",
+				"9xl": "128rem",
+			},
+			borderRadius: {
+				"4xl": "2rem",
+			},
+		},
 	},
-	plugins: [],
 };
