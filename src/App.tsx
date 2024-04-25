d6d1fd5d84f4a1f31d76e4559d71df4a9e6064df
alya1007 <alexandra.konjevic@isa.utm.md>
2024-04-25 18:23:36 +0300

    minor: fix double movies adding in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import { movies } from "./data/movies";
 function App() {
 	localStorage.setItem("movies", JSON.stringify(movies));
 
-	localStorage.setItem("movies", JSON.stringify(movies));
-
 	return (
 		<div className="app bg-slate-200 dark:bg-black h-screen w-full p-6 transition-colors duration-500 ease-in-out">
 			<div className="flex flex-col items-center h-full justify-between">
