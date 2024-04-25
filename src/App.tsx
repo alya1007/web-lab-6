960c068277da433414af1f4fc1611c731f6a5528
alya1007 <alexandra.konjevic@isa.utm.md>
2024-04-25 16:46:31 +0300

    feat(database): populate local storage with movies data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import "./App.css";
+import {movies} from "./data/movies"
 
 function App() {
+	localStorage.setItem("movies", JSON.stringify(movies));
+
 	return (
 		<>
 			<h1 className="text-7xl font-bold underline">Hello world!</h1>
