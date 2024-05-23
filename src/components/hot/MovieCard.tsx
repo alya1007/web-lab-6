be1bb4de2f81481c664300fe5659de50b52a01ea
alya1007 <107748823+alya1007@users.noreply.github.com>
2024-05-23 18:43:13 +0300

    fix(hot-section): adjust styles for white theme (#12)

---
+++ src/components/hot/MovieCard.tsx
@@ -0,0 +1,21 @@
+import { Movie } from "@/data/movies";
+
+const MovieCard = ({ movie }: { movie: Movie }) => {
+	return (
+		<a
+			key={movie.id}
+			className="bg-cover bg-no-repeat h-56 m-5 rounded-lg p-3 flex flex-col justify-between"
+			style={{ backgroundImage: `url('${movie.poster}')` }}
+			href="#"
+		>
+			<h3 className="text-[0.75rem] bg-bgPrimaryWhite/60 dark:bg-bgPrimary/60 px-3 py-0 w-fit rounded-md">
+				{movie.year}
+			</h3>
+			<h3 className="bg-bgPrimaryWhite/60 dark:bg-bgPrimary/60 px-4 py-1 w-fit rounded-lg">
+				{movie.title}
+			</h3>
+		</a>
+	);
+};
+
+export default MovieCard;
